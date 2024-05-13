cc56edad686d131b2be2c89caca23e8ce5987ebe
sam3690 <usamabinayoub@gmail.com>
2024-05-13 10:41:59 -0700

    first commit

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,13 @@
+import React, { Children } from 'react'
+
+const RootLayout = ({children}: {children: React.ReactNode}) => {
+  return (
+    <main>
+        
+        {children}
+        
+    </main>
+  )
+}
+
+export default RootLayout
