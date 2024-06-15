b664296396341a08c59957aeaf7b1828dc41f122
sam3690 <usamabinayoub@gmail.com>
2024-06-15 15:57:55 +0500

    15-06-2024 3:57pm

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,10 +2,11 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import MobileNav from './MobileNav'
+import { SignedIn, UserButton } from '@clerk/nextjs'
 
 const Navbar = () => {
   return (
-    <nav className='flex-between fixed z-50 w-full bg-dark-1py-4 lg:px-10'>
+    <nav className='flex flex-between fixed z-50 w-full bg-dark-1 py-4 lg:px-10'>
       <Link href="/" className='flex items-center gap-1'>
       <Image
         src="/icons/logo.svg"
@@ -14,11 +15,13 @@ const Navbar = () => {
         alt="Yoom Logo"
         className='max-sm:size-10'
         />
-        <p className='text-[26px] font-extra text-white max-sm:hidden'>Yoom</p>
+        <p className='text-[26px] font-extra text-white max-sm:hidden'>Zoom</p>
       </Link>
 
       <div className='flex-between gap-5'>
-        {/* clerk user management */}
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
         <MobileNav />
       </div>
 
