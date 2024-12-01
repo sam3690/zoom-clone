92ea0bd0687819a8570137fea992e7bd018f8f81
sam3690 <usamabinayoub@gmail.com>
2024-12-01 16:15:56 +0500

    01-12-24 4:15 pm

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -7,6 +7,8 @@ import MeetingModal from './MeetingModal'
 import { useUser } from '@clerk/nextjs'
 import { Call, useStreamVideoClient } from '@stream-io/video-react-sdk'
 import { useToast } from "@/components/ui/use-toast"
+import { Textarea } from './ui/textarea'
+import ReactDatePicker from 'react-datepicker';
 
 
 
@@ -65,6 +67,8 @@ const createMeeting = async () => {
     }
 }
 
+const meetingLink = `${process.env.NEXT_PUBLIC_BASE_URL}/meeting/${callDetails?.id}`
+
   return (
     <section className='grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4'>
         <HomeCard 
@@ -97,7 +101,6 @@ const createMeeting = async () => {
             handleClick={() => setMeetingSate('isJoiningMeeting')}
             className='bg-yellow-1'
         />
-
         {!callDetails ? (
             <MeetingModal
                 isOpen={meetingSate === 'isScheduleMeeting'}
